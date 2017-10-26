0f30c8bece1d0a88691d1785aa1b6ff2a7c3356d
Jens Navne <jens.navne@gmail.com>
2017-10-26 12:03:23 +0200

    Add docker build support

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+#DOCKER MAKE ME HAPPY
+
+FROM gradle
+ADD src/ /home/gradle/project/src/
+ADD build.gradle /home/gradle/project/build.gradle
+WORKDIR /home/gradle/project/
+USER root
+RUN chmod -R 777 .
+USER gradle
+RUN gradle clean build
+
+FROM openjdk:8-jre-alpine
+COPY --from=0 /home/gradle/project/build/libs/rolesystem-0.0.1-SNAPSHOT.jar /usr/bin/rolesystem-0.0.1-SNAPSHOT.jar
+ENV SPRING_DATASOURCE_URL=jdbc:postgresql://role-database:5432/postgres \
+    SPRING_DATASOURCE_USERNAME=postgres \
+    SPRING_DATASOURCE_PASSWORD=postgres \
+    SPRING_JPA_GENERATE-DDL=true
+EXPOSE 8080
+CMD ["java", "-jar", "/usr/bin/rolesystem-0.0.1-SNAPSHOT.jar"]
--- src/main/kotlin/dk/group11/rolesystem/RoleSystemApplication.kt
+++ src/main/kotlin/dk/group11/rolesystem/RoleSystemApplication.kt
@@ -1,16 +1,42 @@
 package dk.group11.rolesystem
 
+import org.springframework.beans.BeansException
+import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.jdbc.datasource.AbstractDataSource
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import java.sql.Connection
+import java.sql.SQLException
+import javax.sql.DataSource
 
 @SpringBootApplication
 class RoleSystemApplication {
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    private inner class RetryableDataSourceBeanPostProcessor : BeanPostProcessor {
+        @Throws(BeansException::class)
+        override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
+            var bean = bean
+            if (bean is DataSource) {
+                bean = RetryableDataSource(bean)
+            }
+            return bean
+        }
+
+        @Throws(BeansException::class)
+        override fun postProcessAfterInitialization(bean: Any, beanName: String): Any {
+            return bean
+        }
+    }
 
     @Bean
-    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
-        return BCryptPasswordEncoder()
+    fun dataSouceWrapper(): BeanPostProcessor {
+        return RetryableDataSourceBeanPostProcessor()
     }
 }
 
@@ -18,3 +44,19 @@ fun main(args: Array<String>) {
     SpringApplication.run(RoleSystemApplication::class.java, *args)
 }
 
+
+internal class RetryableDataSource(private val delegate: DataSource) : AbstractDataSource() {
+
+    @Retryable(maxAttempts = 10, backoff = Backoff(multiplier = 2.3, maxDelay = 30000))
+    @Throws(SQLException::class)
+    override fun getConnection(): Connection {
+        return delegate.connection
+    }
+
+    @Retryable(maxAttempts = 10, backoff = Backoff(multiplier = 2.3, maxDelay = 30000))
+    @Throws(SQLException::class)
+    override fun getConnection(username: String, password: String): Connection {
+        return delegate.getConnection(username, password)
+    }
+
+}
