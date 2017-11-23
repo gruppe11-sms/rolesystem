f1f3364c7cca40dd41fb27b46a3e42a675074e82
Zlepper <hansen13579@gmail.com>
2017-11-23 11:33:49 +0100

    Formatting

--- src/main/kotlin/dk/group11/rolesystem/models/ApplicationGroup.kt
+++ src/main/kotlin/dk/group11/rolesystem/models/ApplicationGroup.kt
@@ -4,15 +4,16 @@ import javax.persistence.*
 
 @Entity
 class ApplicationGroup(@Id @GeneratedValue(strategy = GenerationType.AUTO)
-                            var id: Long = 0,
+                       var id: Long = 0,
                        var title: String = "",
                        var description: String = "",
                        @ManyToMany(cascade = arrayOf(CascadeType.ALL))
-                            @JoinColumn
-                            var members: MutableList<ApplicationUser> = mutableListOf(),
+                       @JoinColumn
+                       var members: MutableList<ApplicationUser> = mutableListOf(),
                        @ManyToMany(cascade = arrayOf(CascadeType.ALL))
-                            @JoinColumn
+                       @JoinColumn
                        var roles: MutableList<Role> = mutableListOf()) {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
