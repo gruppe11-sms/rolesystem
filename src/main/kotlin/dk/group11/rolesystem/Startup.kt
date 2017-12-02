4b30bd1cfc24620845863b9cf6b0184b6edde697
Zlepper <hansen13579@gmail.com>
2017-12-02 14:07:22 +0100

    Fix automatic retrying and fix tests

--- src/main/kotlin/dk/group11/rolesystem/Startup.kt
+++ src/main/kotlin/dk/group11/rolesystem/Startup.kt
@@ -7,14 +7,24 @@ import dk.group11.rolesystem.security.COURSE_MANAGEMENT_ROLE
 import dk.group11.rolesystem.security.GROUP_MAINTAINER_ROLE
 import dk.group11.rolesystem.security.ROLE_CREATOR_ROLE
 import dk.group11.rolesystem.services.RoleService
+import org.springframework.beans.BeansException
+import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.jdbc.datasource.AbstractDataSource
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Component
 import java.nio.file.Files
 import java.nio.file.NoSuchFileException
 import java.nio.file.Paths
+import java.sql.Connection
+import java.sql.SQLException
 import java.util.*
+import javax.sql.DataSource
 import javax.transaction.Transactional
 
 private const val SYS_ADMIN_REF: Long = -10
@@ -85,4 +95,39 @@ class Startup(private val roleService: RoleService,
         systemUser.roles.addAll(allRoles)
         userRepository.save(systemUser)
     }
+}
+
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+class RetryableDataSourceBeanPostProcessor : BeanPostProcessor {
+    @Throws(BeansException::class)
+    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
+        var bean = bean
+        if (bean is DataSource) {
+            bean = RetryableDataSource(bean)
+        }
+        return bean
+    }
+
+    @Throws(BeansException::class)
+    override fun postProcessAfterInitialization(bean: Any, beanName: String): Any {
+        return bean
+    }
+}
+
+open internal class RetryableDataSource(private val delegate: DataSource) : AbstractDataSource() {
+
+    @Retryable(maxAttempts = 10, backoff = Backoff(multiplier = 2.3, maxDelay = 30000))
+    @Throws(SQLException::class)
+    override fun getConnection(): Connection {
+        return delegate.connection
+    }
+
+    @Retryable(maxAttempts = 10, backoff = Backoff(multiplier = 2.3, maxDelay = 30000))
+    @Throws(SQLException::class)
+    override fun getConnection(username: String, password: String): Connection {
+        return delegate.getConnection(username, password)
+    }
+
 }
