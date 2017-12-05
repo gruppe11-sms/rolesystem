ed9a8149dc047cf5a84e4a6a48185bf2be896c15
zlepper <hansen13579@gmail.com>
2017-12-05 10:05:36 +0100

    Make expiration date more readable

--- src/main/kotlin/dk/group11/rolesystem/security/SecurityConstants.kt
+++ src/main/kotlin/dk/group11/rolesystem/security/SecurityConstants.kt
@@ -1,6 +1,6 @@
 package dk.group11.rolesystem.security
 
-const val EXPIRATION_TIME: Long = 864000000 // 10 days
+const val EXPIRATION_TIME: Long = 1000 * 60 * 60 * 24 * 10 // 10 days
 const val TOKEN_PREFIX = "Bearer "
 const val HEADER_STRING = "Authorization"
 
